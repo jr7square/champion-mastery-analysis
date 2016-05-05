2a0c0105fe288142d7c820c4bcb1393cc0a4669d
jr7square <recinos@mail.usf.edu>
2016-05-05 11:54:34 -0400

    renaming

---
+++ database/dbtest.js
@@ -0,0 +1,25 @@
+// Testing
+
+//Creating an sqlite3 database with node.js
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
+db.serialize(function() {
+  db.run("CREATE TABLE user( id INT, dt TEXT)");
+  var stmt = db.prepare("INSERT INTO user VALUES (?,?)");
+  for(var i = 0; i < 10; i++){
+    var d = new Date();
+    var n = d.toLocaleTimeString();
+    stmt.run(i,n);
+  }
+  stmt.finalize();
+  db.each("SELECT id, dt FROM user", function(err,row){
+    if(err){
+      console.log("there was an error");
+    }
+    else{
+      console.log("User id : " + row.id, row.dt);
+    }
+  });
+});
+
+db.close();
